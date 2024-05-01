a4fbfc1ffef6c6370fb4b90dc23a2b4ffa7b7c23
davidegualtieri02 <davidegualtieri02@gmail.com>
2024-05-01 20:30:40 +0200

    Implementazione classe config.php

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+//Connessione al database
+define('DB_NAME', 'prova'); //Definisco la costante DB_NAME
+define('DB_HOST', 'localhost'); //Definisco la costante DB_HOST
+define('DB_USER', 'root'); //Definisco la costante DB_USER
+define('DB_PASS', 'password123'); //Definisco la costante DB_PASS
+//Tutte le costanti definite in precedenza vengono utilizzate in /Foundation/FEntityManager.php
