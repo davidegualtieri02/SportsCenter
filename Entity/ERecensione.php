4f1d0e0bc3ce242cc52c14415e209e44768d3f10
davidegualtieri02 <davidegualtieri02@gmail.com>
2024-06-03 18:12:18 +0200

    Completamento classe ERecensione e implementazione classe FRecensione

--- Entity/ERecensione.php
+++ Entity/ERecensione.php
@@ -1,16 +1,31 @@
 <?php
 class ERecensione{
-    private int $valutazione;
-    private string $commento;
-    private $image;
     private int $idRecensione;
+    private string $commento;
+    private int $valutazione;
+    private DateTime $DataOra;
+    private $removed = false;
+    protected EUtente $utente;
+    private $images;
     private static $entità = ERecensione::class;
 
-    public function __construct($valutazione,$commento,$image,$idRecensione){
+    public function __construct($valutazione,$commento){
         $this->valutazione = $valutazione;
         $this->commento = $commento;
-        $this->image=$image;
-        $this->id_recensione = $idRecensione;
+        $this->setTime();
+        $this->images= [];
+    }
+
+    public static function getEntità(): string{
+        return self::$entità;
+    }
+
+    public function getId(){
+        return $this->idRecensione;
+    }
+
+    public function setId($id){
+        $this->idPost = $id;
     }
     public function getValutazione(){
         return $this->valutazione;
@@ -24,14 +39,63 @@ class ERecensione{
     public function setCommento($commento){
         $this->commento = $commento;
     }
-    public function getImage(){
-        return $this->image;
+
+    public function getTime(){
+        return $this->DataOra;
+    }
+
+    public function setTime(){
+        $this->DataOra = new DateTime("now");
+    }
+
+    public function getTimeStr(){
+        return $this->DataOra->format('D-m-y H:i:s');
+    }
+
+    public function setDataOra($DataOra){
+        $this->DataOra = $DataOra;
+    }
+
+    public function isBanned(): bool{
+        return $this->removed;
+    }
+
+    public function setBan(bool $removed): void{
+        $this->removed = $removed;
+    }
+
+    public function getUtente(){
+        return $this->utente;
+    }
+
+    public function setUtente(EUtente $utente): void{
+        $this->utente = $utente;
+    }
+
+    public function getImages(){
+        return $this->images;
     }
     public function setImage($image){
         $this->foto = $image;
     }
-    public static function getEntità(){
-        return self::$entità;
+
+    public function addImage(EImage $image): void{
+        $idImg = $image->getId();
+
+        //Ora controllo se l'immagine è già presente nell'array tramite il suo ID
+        $imageExists = false;
+        foreach($this->images as $existingImage){
+            if ($existingImage->getId() === $idImg){
+                $imageExists = true;
+                break; //interrompi l'esecuzione del ciclo se viene trovato un ID corrispondente
+            }
+        }
+
+        //Se l'immagine non esiste nell'array, aggiungila
+        if (!$imageExists){
+            $this->images[] = $image;
+            $image->setRecensione($this);
+        }
     }
      //metodo toString esiste predefinito
 }
--- Foundation/FRecensione.php
+++ Foundation/FRecensione.php
@@ -1,5 +1,42 @@
 <?php
 
 class FRecensione{
-    
+    private static $tabella = "recensione";
+    private static $valore = "(NULL, :commento, :valutazione, :DataOra, :removed, :id_Utente)";
+    private static $chiave = "idRecensione";
+    public static function getTabella(){
+        return self::$tabella;
+    }
+    public static function getValore(){
+        return self::$valore;
+    }
+    public static function getClasse(){
+        return self::class;
+    }
+
+    public static function getChiave(){
+        return self::$chiave;
+    }
+
+    public static function CreaOggRecensione($risultatoQuery){
+        if(count($risultatoQuery) > 0){
+            $recensioni = array();
+            for($i = 0; $i < count($risultatoQuery); $i++){
+                $rec = new ERecensione($risultatoQuery[$i]['commento'],$risultatoQuery[$i]['valutazione']);
+                $rec->setId($risultatoQuery[$i]['idRecensione']);
+                $DataOra = DateTime::createFromFormat('D-m-y H:i:s', $risultatoQuery[$i]['DataOra']);
+                $rec->setDataOra($DataOra);
+                $rec->setBan($risultatoQuery[$i]['removed']);
+                $recensioni[] = $rec;
+            }
+            return $recensioni;
+        }else{
+            return array();
+        }
+    }
+
+    public static function bind($dichiarazione, $recensione){
+        $dichiarazione->bindValue(":commento", $recensione->getCommento(), PDO::PARAM_STR);
+        
+    }
 }
