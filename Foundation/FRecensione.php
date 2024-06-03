5d826786061213c430410bc9f0b5641d92089ddb
davidegualtieri02 <davidegualtieri02@gmail.com>
2024-06-03 22:15:43 +0200

    Continuo classe FRecensione

--- Foundation/FRecensione.php
+++ Foundation/FRecensione.php
@@ -37,6 +37,81 @@ class FRecensione{
 
     public static function bind($dichiarazione, $recensione){
         $dichiarazione->bindValue(":commento", $recensione->getCommento(), PDO::PARAM_STR);
-        
+        $dichiarazione->bindValue(":valutazione", $recensione->getValutazione(), PDO::PARAM_INT);
+        $dichiarazione->bindValue(":DataOra", $recensione->getDataOra(), PDO::PARAM_STR);
+        $dichiarazione->bindValue(":removed", $recensione->isBanned(), PDO::PARAM_BOOL);
+        $dichiarazione->bindValue(":id_Utente", $recensione->getUtente()->getId(), PDO::PARAM_INT);
     }
+
+    public static function getOgg($id){
+        $risultato = FEntityManager::getIstanza()->recuperaOggetto(self::getTabella(), self::getChiave(), $id);
+        if(count($risultato) > 0){
+            $recensione = self::CreaOggRecensione($risultato);
+            return $recensione;
+        }else{
+            return null;
+        }
+    }
+
+    public static function salvaOgg($ogg, $fieldArray = null){
+        if($fieldArray === null){
+            $salvaRecensione = FEntityManager::getIstanza()->salvaOgg(self::getClasse(), $ogg);
+            if($salvaRecensione !== null){
+                return $salvaRecensione;
+            }else{
+                return false;
+            }
+        }else{
+            try{
+                FEntityManager::getIstanza()->getdb()->beginTransaction();
+                foreach($fieldArray as $fv){
+                    FEntityManager::getIstanza()->updateOgg(FRecensione::getTabella(), $fv[0], $fv[1], self::getChiave(), $ogg->getId());
+                }
+                FEntityManager::getIstanza()->getdb()->commit();
+                return true;
+            }catch(PDOException $errore){
+                echo "ERROR" . $errore->getMessage();
+                FEntityManager::getIstanza()->getdb()->rollBack();
+                return false;
+            }finally{
+                FEntityManager::getIstanza()->chiusuraConnessione();
+            }
+        }
+    }
+
+    public static function eliminaRecensioneDalDB($idRecensione, $id_Utente){        
+        try{
+            FEntityManager::getIstanza()->getdb()->beginTransaction();
+            $queryResult = FEntityManager::getIstanza()->recuperaOggetto(self::getTabella(), self::getChiave(), $idRecensione);
+
+            if(FEntityManager::getIstanza()->esisteNelDb($queryResult) && FEntityManager::getIstanza()->verificaCreatore($queryResult, $id_Utente)){
+
+                $imagesList = FEntityManager::getIstanza()->recuperaOggetto(FImage::getTabella(), self::getChiave(), $idRecensione);
+                for($i = 0; $i < count($imagesList); $i++){
+                    FEntityManager::getIstanza()->deleteOggInDb(FImage::getTabella(), FImage::getChiave(), $imagesList[$i][FImage::getChiave()]);
+                }
+
+                $reportList = FEntityManager::getIstanza()->recuperaOggetto(FReport::getTable(), self::getChiave(), $idRecensione);
+                for($i = 0; $i < count($reportList); $i++){
+                    FEntityManager::getIstanza()->deleteOggInDb(FReport::getTabella(), FReport::getChiave(), $reportList[$i][FReport::getChiave()]);
+                }
+
+                FEntityManager::getIstanza()->deleteOggInDb(self::getTabella(), self::getChiave(), $idRecensione);
+
+                FEntityManager::getIstanza()->getdb()->commit();
+                return true;
+            }else{
+                FEntityManager::getIstanza()->getdb()->commit();
+                return false;
+            }
+        }catch(PDOException $errore){
+            echo "ERROR " . $errore->getMessage();
+            FEntityManager::getIstanza()->getdb()->rollBack();
+            return false;
+        }finally{
+            FEntityManager::getIstanza()->chiusuraConnessione();
+        }
+    }
+
+    
 }
