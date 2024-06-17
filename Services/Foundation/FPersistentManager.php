c7673e9232895f18e442ab576981abb23ca4a67d
lorenzofracassi <lorenzo.fracassi@student.univaq.it>
2024-06-17 10:45:55 +0200

    Inizio implementazione Persistent Manager

---
+++ Services/Foundation/FPersistentManager.php
@@ -0,0 +1,43 @@
+<?php
+class FPersistentManager{
+    /**
+     * Singleton Class
+     */
+    private static $istanza;
+
+
+    private function _construct(){
+    // non può essere creato un oggetto di questa classe al di fuori della classe stessa siccome il costruttore è privato
+    //in tale modo si implementa il pattern singleton, in cui abbiamo una sola istanza dell'oggetto Persistent Manager contenuta nella classe stessa PersistentManager.
+    }
+    /**
+     * Metodo per creare un'istanza del PersistentManager
+     */
+    public static function getIstanza(){// self si riferisce alla classe stessa PersistentManager
+        if(!self::$istanza){ // se $istanza è null, cioè non è stata creata in passato , viene creata una nuova istanza e viene ritornata l'istanza
+            self::$istanza = new self();
+        }
+        return self::$istanza; // se l'istanza esiste gia viene ritornata semplicemente l'istanza , senza che il metodo getIstanza() ne crei una nuova 
+    }
+
+    //---------------------------------------Direttamente con l'Entity Manager-----------------------------------------------------------------
+
+
+    /**
+     * metodo che restituisce un oggetto specificando la classe e un id 
+     * @param string $classe si riferisce alla classe entity di appartenenza dell'oggetto da prelevare dal db
+     * @param int $id si riferisce all'id dell'oggetto 
+     * @return mixed
+     */
+    public static function recuperaOggetto($classe,$id){ // $classe contiene il nome della classe passata come parametro alla funzione 
+        $classeFound = "F" . substr($classe,1); 
+        //substr($classe,1) estrae una sottostringa dal nome della classe , eliminando il primo carttere e tenendo tutti gli altri. Se per esempio la classe è EUtente , il metodo rimuove la E , ottenendo Utente 
+        //e al posto della E , viene posto la lettera F in modo che la classe sarà una classe del paacchetto Foundation .
+        $metodoStatico = " getOgg";
+        $risultato = call_user_func([$classeFound,$metodoStatico],$id);
+        // call_user_func permette di chiamare una funzione o metodo spercificato . in questo caso chiama il metodo contenuto in $metodoStatico
+        //che è getOgg della classe $classefound che è una delle classi contenute in foundation. $id è l'ID dell'oggetto della classe, che viene usato per prendere tale oggetto dal db.
+        // l'id specificato ci serve per prebdere quell'oggetto dal db tramite il suo ID .
+        return $risultato;// il risultato è una tupla che contiene l'oggetto , essa viene ritornata in array associativo in cui ogni valore di un campo è un elemento dell'array e i campi sono le chiavi degli elementi dell'array
+    }
+}
