434840bec49525e405534afe070faa02706f4611
dierom0 <diegoromanelli0@icloud.com>
2024-06-21 09:53:38 +0200

    CPrenotaCampo

--- Controller/CPrenotaCampo.php
+++ Controller/CPrenotaCampo.php
@@ -1,10 +1,11 @@
 <?php
 
-class CPrenotaCampo(){
-    if(CUtente::Loggato()){
-        $id_Utente = USession::getIstanza()->getElementoSessione('utente'); //se l'utente è loggato, recupera il suo id dalla sessione
-        $utente = FPersistentManager::getIstanza()->recuperaOgg(FUtente::getClasse(), $id_Utente); //utente o utente registrato?. recupera l'oggetto utente tramite il suo id
-        //...
+class CPrenotaCampo{
+    public static function prenotacampo(){
+        if(CUtente::Loggato()){
+            $id_Utente = USession::getIstanza()->getElementoSessione('utente'); //se l'utente è loggato, recupera il suo id dalla sessione
+            $utente = FPersistentManager::getIstanza()->recuperaOgg(FUtente::getClasse(), $id_Utente); //utente o utente registrato?. recupera l'oggetto utente tramite il suo id
+            //...
+        }
     }
-    
 }
