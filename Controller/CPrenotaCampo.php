44ec9f4f63741f4f426c9a22f02d6cdd73da4812
dierom0 <diegoromanelli0@icloud.com>
2024-06-21 09:54:24 +0200

    CPrenotaCampo syntaxerror

--- Controller/CPrenotaCampo.php
+++ Controller/CPrenotaCampo.php
@@ -4,7 +4,7 @@ class CPrenotaCampo{
     public static function prenotacampo(){
         if(CUtente::Loggato()){
             $id_Utente = USession::getIstanza()->getElementoSessione('utente'); //se l'utente è loggato, recupera il suo id dalla sessione
-            $utente = FPersistentManager::getIstanza()->recuperaOgg(FUtente::getClasse(), $id_Utente); //utente o utente registrato?. recupera l'oggetto utente tramite il suo id
+            $utente = FPersistentManager::getIstanza()->recuperaOggetto(FUtente::getClasse(), $id_Utente); //utente o utente registrato?. recupera l'oggetto utente tramite il suo id
             //...
         }
     }
