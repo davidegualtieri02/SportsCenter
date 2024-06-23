85eda8e62d31d6c229430430d42d2ba1ca2807c0
lorenzofracassi <lorenzo.fracassi@student.univaq.it>
2024-06-23 10:46:12 +0200

    Modifiche metedo ScriviRecensione in CRecensione

--- Controller/CRecensione.php
+++ Controller/CRecensione.php
@@ -16,7 +16,7 @@ class CRecensione{
                 if(Userver::getRichiestaMetodo() =="GET"){
                     //se il metodo di richiesta è GET viene mostrato il form per una nuova recensione, cioè il server manda al browser dell'utente  la form per recensire il campo
                     $view->formNuovaRecensione($utente,$idcampo,null);
-                }//se la richiesta è POST l'utente scrive il commento e aggiunge l'immagine/i del campo nel commento 
+                }//se la richiesta è POST l'utente scrive il commento e aggiunge la recensione al db
                 elseif(UServer::getRichiestaMetodo()=="POST"){
                     $valutazione = VRecensione::getValutazione();
                     $messaggio = VRecensione::getMessaggio();
@@ -24,35 +24,40 @@ class CRecensione{
                     $recensione = new ERecensione($valutazione,$messaggio,$utente->getId(),$idcampo,$ora)
                     $pm::uploadOgg($recensione);
                     // carica l'immagine dal browser dell'utente fino al server
-                    if(isset(UMetodiHTTP::files('immagini'))){
-                        $immagini = UMetodiHTTP::files('immagini');
+                    if(isset(UMetodiHTTP::files('immagini'))){ //tramite isset() vediamo se nell'array $_FILES sono state caricate già le immagini con chiave 'immagini'. isset() restituisce true se le immagini sono state caricate nella recensione
+                        $immagini = UMetodiHTTP::files('immagini'); // se isset() rida true, $immagini contiene le informazioni sulle immagini appena caricate
+                        // tali informazioni sono : i nomi originali dei file (immagini) caricate , il formato delle immagini se jpeg o image , il percorso temporaneo in cui le immagini sono state salvate sul server, le dimensioni delle immagini 
+                        // e contiene anche i codici di errore associati al caricamento di ciascun file
                      }
-                    
-                    foreach ($immagini['tmp_name'] as $index => $percorsoTemporaneo) {
-                        if ($immagini['error'][$index] === UPLOAD_ERR_OK) {
-                            // Aggiungi l'immagine alla recensione e ottieni il percorso dell'immagine salvata
-                            $percorsoImmagine = FPersistentManager::AggiungiImmagini($percorsoTemporaneo);
-                            //aggiungi l'immagine all'oggetto ERecensione
-                            $recensione ->setImages($percorsoImmagine);
-                            
+                    // per ogni immagine caricata il ciclo foreach itera per ogni file temporaneo caricato e associato uno alla volta alla chiave 'tmp_name' nell'array immagini
+                    //$immagini['tmp_name'] è un array che contiene i percorsi temporanei dei file caricati sul server. SICCOME IL FILE E' STATO CARICATO , CONOSCENDO IL PERCORSO TEMPORANEO DEL FILE NEL SERVER POSSIMAO
+                    //SPOSTARE TALE IMMAGINE DAL PERCORSO TEMPORANEO E PORLA NELLA RECENSIONE.
+                    // l'array che contiene i percorsi delle immagini è proprio $immagini[tmp_name]
+                    foreach ($immagini['tmp_name'] as $index => $percorsoTemporaneo) {//$index assume una alla volta tutti i valori delle chiavi dell'array $immagini e quindi $percorsoTemporaneo assume una alla volta tutti i valori delle chiavi assunte da $index, cioè assume come valori i valori dei percorsi temporanei delle immagini caricate nel server .
+                        //index assume come valore un id dei percorsi , perchè nell'array abbiamo chiave(id)=>percorso
+                        if ($immagini['error'][$index] === UPLOAD_ERR_OK) { //UPLOAD_ERR_OK verifica che i file sono stati caricati senza errori
+                            // Aggiunge l'immagine all'oggetto ERecensione  e salva la recensione
+                            $pm= FPersistentManager::AggiungiImmagini($recensione,$percorsoTemporaneo);
                         }
 
                     }
                     // Aggiorna la recensione nel database con le immagini aggiunte
-                    FPersistentManager::getInstance()->updateOgg($recensione);
+                    $pm::updateOgg($recensione);
             
                     header("Location: /percorso/di/destinazione");
                      exit;
-        
-                } else {
-                    header('Location: /percorso/login');
-                     exit;
+                //l'utente non ha prenotato questo campo , viene mostrato un messaggio di errore
+                }else{
+                    $view ->mostraErrore("Non hai prenotato questo campo e non puoi recensirlo");
                 }
-
             }
-        }
+        }else{
+            //l'utente non è loggato ,reindirizza alla pagina di login
+            header('Location : /SportsCenter/login');
 
+        }
     }
+
     public static function eliminaRecensione($idRecensione) {
         // Verifica se l'utente è loggato
         if (CUtente::Loggato()) {
--- Controller/CTesseramento.php
+++ Controller/CTesseramento.php
@@ -10,6 +10,8 @@ class CTesseramento{
             //recupera l'id dell'utente dalla sessione
             $id_utente = USession::getIstanza()->getElementoSessione('Utente');
             //...la funzione creaOggTessera in FTessera prende in input una query che contiene cosa?
+            //allora daieg , il risultato della query è una o + tuple che contiengono i dati di una o + tessere , che vengono   restituite in un array multidimensionale dove ogni elemento di questo array è una tessera .  I dati della tessera sono allora volta inseriti in un altro array
+            //quindi l'array multidimensionale può contentere altri  array con + tessere
         }
     }
 }
