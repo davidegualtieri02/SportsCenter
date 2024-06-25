abd014c2862d3d5ae2e3f58e778b76f9e983da9e
lorenzofracassi <lorenzo.fracassi@student.univaq.it>
2024-06-25 12:46:02 +0200

    inizio implementazione VPrenotaCampo

--- View/VPrenotaCampo.php
+++ View/VPrenotaCampo.php
@@ -2,6 +2,22 @@
 class VPrenotaCampo{
     private function __construct(){
          $this->smarty = StartSmarty::configuration();
+        }
+   public function getTitolo(){
+    return UMetodiHTTP::post('titoloCampo');
+    }
+   public function getPrezzo(){
+    return UMetodiHTTP::post('prezzo');
+    }
+
+   public function MostraFormPrenotazione($campo){
+    if(CUtente::Loggato()){
+        $this->smarty->assign('UtenteLoggato','Loggato');
+   }
+   else{$this->smarty->assign('UtenteLoggato','Loggato');
+    $this->smarty->assign('Campo',$campo);
+    $this->smarty->display('./Smarty/libs/templates/pagamento.tpl');
 
     }
+   }
 }
