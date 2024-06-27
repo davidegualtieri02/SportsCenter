f4a5619e3a941f4ce9da4e1413431f9f19e0de98
lorenzofracassi <lorenzo.fracassi@student.univaq.it>
2024-06-27 10:37:27 +0200

    prova

--- Entity/prova.php
+++ Entity/prova.php
@@ -5,7 +5,7 @@ include ("ECampo_Pallavolo.php");
 
 $immagine = new EImage("prova"," 100"," jpeg"," 1900",'1','1');
 $campopallavolo = new ECampo_Pallavolo('1',"fuori",'1'," gomma",$immagine);
-echo $campopallavolo->getIdCampoPallavolo();
+print_r($campopallavolo);
  
 
 
