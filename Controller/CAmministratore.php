115c2f355529fb7cd85a852dfe2964887ddeda4e
lorenzofracassi <lorenzo.fracassi@student.univaq.it>
2024-06-28 18:29:44 +0200

    implementazione annullaPrenotazioneAmm() e MostraConfermaPrenotazioneAmm() nella classe CAmministratore

--- Controller/CAmministratore.php
+++ Controller/CAmministratore.php
@@ -104,56 +104,38 @@ class CAmministratore{
     }
 
      /**
-     * Metodo per prenotare un campo sportivo
+     * Metodo per confermare la prenotazione
      * @param $idCampo è l'id del campo che l'utente vuole prenotare
      */
-    public static function prenotaCampo($idCampo){ //Con GET il server invia la form di prenotazione 
+    public static function MostraConfermaPrenotazioneAmm(){
+        $sessione = USession::getIstanza();
         $pm = FPersistentManager::getIstanza();
-        $sessione = USession::getIstanza(); // otteniamo un'istanza della sessione utente
-        $campo = $pm::uploadOgg($idCampo);// carichiamo l'oggetto campo sportivo nel db
-        $view = new VPrenotaCampo();//creiamo un'istanza della view per la prenotazione del campo
-        if(UServer::getRichiestaMetodo()=="GET"){//verifichiamo se la richiesta al server è di tipo GET, cioè manda i dati dal server al client , il server manda i dati sui campi disponibili all'amministratore che sta effettuando la prenotazione
-            if(CAmministratore::Loggato()){// se l'utente è loggato
-                $utente = unserialize($sessione->LeggiValore('utente'));//ripristina una stringa in un oggetto. Quindi 'utente' viene trasformato da stringa a oggetto utente 
-                $view ->MostraFormPrenotazione($campo);//viene mostrata la form per la prenotazione
-            }else{// se l'amministratore non è loggato viene reindirizzato alla pagina di login 
-                header('Location: /SportsCenter/Amministratore/login');
-                exit;//fa in modo che il codice dopo non viene eseguito se l'utente non è loggato.
-            }    
-        }
-        if (UServer::getRichiestaMetodo()=="POST"){//Con POST l'utente che prenota i campi invia i dati della prenotazione al server per vedere se sono disponibili
+        $view = new VPrenotaCampo();
+        $campo = $_POST['campo'];
+        $data = $_POST['data'];
+        $orario = $_POST ['orario'];
+        $attrezzatura = $_POST['id_attrezzatura'];
+        if(UServer::getRichiestaMetodo()=="GET"){
             if(CAmministratore::Loggato()){
-                // l'amministratore invia per l'utente la data e l'orario  in cui vorrebbe prenotare il campo al server 
-                // a prescinde dall'utente che prenota il campo
-                $amministratore = unserialize($sessione->LeggiValore('utente'));
-                $idAmm = $amministratore->getId();
-                $data = UMetodiHTTP::post('data'); 
-                $orario = UMetodiHTTP::post('orario');
-                //e invia anche l'attrezzatura che vorrebbe prenotare l'utente
-                $idAttrezzatura = UMetodiHTTP::post('id_attrezzatura');
-                $pdo = new PDO('mysql:host=localhost;dnname =prova','root','password123', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,PDO::ATTR_EMULATE_PREPARES => false]);
-                if(FPersistentManager::campoDisponibile($pdo,$idCampo,$data,$orario)){
-                    //eseguiamo l'inserimento della prenotazione nel database
-                    $sql = "INSERT INTO 'Prenotazione' ('data','orario','id_campo','id_attrezzatura','id_utente') VALUES (:data,:orario,:id_campo,:id_attrezzatura,:id_utente)";
-                    $dichiarazione = $pdo->prepare($sql);
-                    $dichiarazione->execute([':id_utente'=>$idAmm,':id_campo' => $idCampo,':data'=> $data,':orario'=>$orario,':id_attrezzatura'=>$idAttrezzatura]);
-                    $view->MostraMessaggioConferma("Campo prenotato con successo!");
-                }
-                else{
-                    $view->MostraMessaggioErrore("Il campo non è disponibile per la prenotazione");
-                }    
-        
-            }else{
-                header('Location: /SportsCenter/Amministratore/login');
-                exit;
+                $amm = unserialize($sessione->LeggiValore('Amministratore'));
+                $view ->MostraForm($amm,$campo,$data,$orario,$attrezzatura);
             }
         }
+        elseif(UServer::getRichiestaMetodo()=="POST"){
+            $amministratore = unserialize($sessione->LeggiValore('Utente'));
+            $prenotazione = new EPrenotazione($data,$orario,true,$campo->getId_campo(),$attrezzatura);
+            $pm::uploadOgg($prenotazione);
+            $view->ConfermaPrenotazioneAmministratore($amministratore);
+
+
+        }
+
     }
     /**
      * Metodo per annullare una prenotazione
      * @param $idPrenotazione è l'id della prenotazione che l'utente vuole annullare
      */
-    public static function annullaPrenotazione($idPrenotazione) {
+    public static function annullaPrenotazioneAmm($idPrenotazione) {
         $pm = FPersistentManager::getIstanza();
         $sessione = USession::getIstanza();
         $view = new VPrenotaCampo();
@@ -163,21 +145,8 @@ class CAmministratore{
                 $pdo = new PDO('mysql:host=localhost;dnname =prova','root','password123', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,PDO::ATTR_EMULATE_PREPARES => false]);
                 // Richiama il metodo isUserBookingOwner dalla classe CBooking
                 if ($pm::VerificaUtenteprenotazione($pdo, $idPrenotazione, $amministratore->getId())) {
-                    $sql = "DELETE FROM Prenotazione WHERE id_prenotazione = :id_prenotazione AND id_utente = :id_utente";
-                    $dichiarazione = $pdo->prepare($sql);
-                    $dichiarazione->execute([
-                        ':id_prenotazione' => $idPrenotazione,
-                        ':id_utente' => $amministratore->getId()
-                    ]);
-
-                    // Controlla se la cancellazione è avvenuta , DELETE restituisce le righe rimosse e rowCount conta tali righe 
-                    if ($dichiarazione->rowCount() > 0) {//se l'array dichiarazione contiene almeno una riga cioè quando viene eliminato una riga , $dichiarazione contiene l'elemento eliminato e dunque ha + di 0 elementi 
-                        $view->MostraMessaggioConferma("Prenotazione annullata con successo!"); // se l array $dichiarazione ha più di 0 elementi  allora l'eliminazione della prenotazione è avvenuta con successo
-                    } else {
-                        $view->MostraMessaggioErrore("Errore nell'annullamento della prenotazione.");// se la prenotazione non viene rimossa , viene printato questo messaggio 
-                    }
-                } else {
-                    $view->MostraMessaggioErrore("Non hai i permessi per annullare questa prenotazione."); // se rowCount=0 non viene rimossa la prenotazione , cioè l'utente non ha prenotato nessuna prenotazione e dunque non la può eliminare
+                    FPersistentManager::deleteOgg('Prenotazione',$idPrenotazione,'id_prenotazione');
+                    $view->MostraMessaggioConferma("Prenotazione annullata con successo!");
                 }
             } else {
                 header('Location: /SportsCenter/Utente/login');
