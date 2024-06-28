d7d14e709f7b50c8696bdd26a89157fc21614266
dierom0 <diegoromanelli0@icloud.com>
2024-06-28 11:01:07 +0200

    Modifiche FEntityManager per risolvere errori connessione al DB.

--- Services/Foundation/FEntityManager.php
+++ Services/Foundation/FEntityManager.php
@@ -10,6 +10,8 @@
 //4)Gestione delle risorse: Il singleton può gestire la chiusura automatica della connessione al database quando non è più necessaria.
 // Questo aiuta a evitare perdite di risorse e a mantenere pulito il codice.
 
+require_once(__DIR__."/../../config/config.php"); //include config.php per i dati del DB
+
 class FEntityManager{
     // dovrebeb essere un singleton ma anche nel progetto Agora non viene specificato il fatto che sia un singleton 
     //come dovrebbe essere in realtà
@@ -23,7 +25,7 @@ class FEntityManager{
             // di connessione al database relazionale utilizzando la classe PDO.PDO è una classe predefinita PHP che fornisce un'interfaccia per comunicare  con
             //database relazionali . E'una parte fondamentale per eseguire query e interagire con il database.
             //("mysql:dbname ...ect) è il parametro che viene passato al costruttore della classe PDO.
-            self::$db = new PDO ("mysql:dbname=".DB_NAME.";host=".DB_HOST.";charset=utf8;",DB_USER,DB_PASS); // DB_USER e DB_PASS sono le credenziali di accesso al database.
+            self::$db = new PDO("mysql:dbname=".DB_NAME.";host=".DB_HOST.";charset=utf8", DB_USER, DB_PASS); // DB_USER e DB_PASS sono le credenziali di accesso al database.
             //la stringa dentro le parentesi specifica il tipo di database in questo caso Mysql , il nome del database(DB_NAME), l'host(DB_HOST) e il set di caratteri per
             // per la connessione .Questo set definisce l’insieme dei caratteri che possono essere memorizzati e identificati nel database durante le operazioni di inserimento, estrazione e manipolazione dei dati
             //Se la connessione ha successo l'oggetto $db conterrà la connessione attiva al database
@@ -71,7 +73,7 @@ class FEntityManager{
      */
     public static function recuperaOggetto($tabella,$campo,$id){ 
         try{
-            $query = "SELECT * FROM".$tabella. "WHERE".$campo."=".$id .";";
+            $query = "SELECT * FROM ".$tabella. " WHERE ".$campo."=".$id .";";
             $dichiarazione =self::$db->prepare($query);//questa riga sta preparando la query SQL per la propria esecuzione 
             // self::$db si riferisce all'oggetto $db che rappresenta una connessione al database
             //l'oggetto dichiarazione viene creato quando si prepara una query SQL utilizzando il metodo prepare() di un oggetto PDO
--- Services/Foundation/prova.php
+++ Services/Foundation/prova.php
@@ -1,1 +1,43 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+//include("FUtente.php");
+include("FEntityManager.php");
+
+//EntityManager::$db = new PDO("mysql:dbname=Prova;host=localhost;charset=utf8", "root");
+
+
+//new PDO("mysql:dbname=Prova;host=localhost;charset=utf8", "root");
+
+//$db = new PDO("mysql:dbname=Prova;host=localhost;charset=utf8", "root");
+
+//FUNZIONA
+$ogg1 = FEntityManager::getIstanza();
+print_r($ogg1);
+
+//FUNZIONA
+$ogg2 = FEntityManager::recuperaOggetto("Utente", "id_utente", 1); 
+print_r($ogg2);
+
+//u->recuperaOggetto("Utente","id_utente",1);
+
+//print_r(FUtente::verifica("id_utente", 1));
+
+
+/*
+$query = "SELECT * FROM Utente WHERE id_utente = 1";
+$dichiarazione =FEntityManager::$db->prepare($query);
+$dichiarazione->execute();
+$countRighe = $dichiarazione->rowCount();
+if($countRighe >0){
+	$risultato = array();
+	$dichiarazione->setFetchMode(PDO::FETCH_ASSOC);
+	while ($righe = $dichiarazione->fetch()){
+		$risultato[] = $righe;
+	}
+    return $risultato;
+ }else{
+    return array();
+}
+*/
