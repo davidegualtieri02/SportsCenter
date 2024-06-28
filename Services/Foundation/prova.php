8fb8771bff05e2ca025619a80636567255266888
dierom0 <diegoromanelli0@icloud.com>
2024-06-28 11:38:20 +0200

    prova.php

--- Services/Foundation/prova.php
+++ Services/Foundation/prova.php
@@ -2,7 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-//include("FUtente.php");
 include("FEntityManager.php");
 
 //EntityManager::$db = new PDO("mysql:dbname=Prova;host=localhost;charset=utf8", "root");
@@ -19,6 +18,22 @@ print_r($ogg1);
 //FUNZIONA
 $ogg2 = FEntityManager::recuperaOggetto("Utente", "id_utente", 1); 
 print_r($ogg2);
+
+//FUNZIONA
+$ogg3 = FEntityManager::recuperaTuple("Utente");
+print_r($ogg3);
+
+/*
+$u1 = 
+$ogg4 = FEntityManager::SalvaOgg("FUtenteRegistrato", $u1);
+
+/*da rivedere
+include(__DIR__."/../../Entity/EUtenteRegistrato.php");
+include("FUtenteRegistrato.php");
+
+$u1 = new EUtenteRegistrato("Diego", "Romanelli", "diego@email.it", "diegopw", false);
+$r = FUtenteRegistrato::salvaOgg($u1);
+*/
 
 //u->recuperaOggetto("Utente","id_utente",1);
 
