49a58fd73d222c9a75121f3eee9db815c1654459
davidegualtieri02 <davidegualtieri02@gmail.com>
2024-07-08 23:37:15 +0200

    Aggiunta commenti in provaCompilazione_templates_c.php

--- smarty/libs/provaCompilazione_templates_c.php
+++ smarty/libs/provaCompilazione_templates_c.php
@@ -1,7 +1,18 @@
 
 <?php
+
+
+// ATTENZIONE!!!!!!
+// Non è necessario runnare questo file quando vengono
+// modificati dei file template oppure all'aggiunta di
+// nuovi file template, è un processo che dovrebbe, e
+// sottolineo dovrebbe, svolgere smarty automaticamente.
+
 include'Smarty.class.php';
 $smarty = new Smarty;
-// forza la compilazione di tutti i file di template
+// forza la compilazione dei file di template
+// il primo parametro indica l'estensione dei file da considerare
+// quando il secondo parametro è FALSE => SOLO FILES MODIFICATI
+// quando il secondo parametro è TRUE => TUTTI I FILES
 $smarty->compileAllTemplates('.tpl', true);
 
