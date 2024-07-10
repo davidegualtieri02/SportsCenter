9bf9efdcdb964fcf50efbdb8840cca6bb7be29fa
dierom0 <diegoromanelli0@icloud.com>
2024-07-10 17:27:53 +0200

    config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
 define('DB_NAME', 'DB'); //Definisco la costante DB_NAME
 define('DB_HOST', 'localhost'); //Definisco la costante DB_HOST
 define('DB_USER', 'root'); //Definisco la costante DB_USER
-define('DB_PASS', ' '); //Definisco la costante DB_PASS
+define('DB_PASS', ''); //Definisco la costante DB_PASS
 define('SQL_FILE_PATH', 'sportscenter.sql');
 //Tutte le costanti definite in precedenza vengono utilizzate in /Foundation/FEntityManager.php
 
