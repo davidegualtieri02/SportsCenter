50fe5c595624a08ccfe87dcf30023bfa55633f2a
lorenzofracassi <lorenzo.fracassi@student.univaq.it>
2024-07-10 17:14:09 +0200

    voto

--- config/config.php
+++ config/config.php
@@ -7,3 +7,13 @@ define('DB_USER', 'root'); //Definisco la costante DB_USER
 define('DB_PASS', ' '); //Definisco la costante DB_PASS
 define('SQL_FILE_PATH', 'sportscenter.sql');
 //Tutte le costanti definite in precedenza vengono utilizzate in /Foundation/FEntityManager.php
+
+//Web APP parameter for custom app
+define('MAX_VIP', 3);
+define('MAX_POST_EXPLORE', 10);
+define('MAX_WARNINGS', 3);
+define('MAX_IMAGE_SIZE', 5242880); // 5MB
+define('ALLOWED_IMAGE_TYPE',['image/jpeg', 'image/png', 'image/jpg']);
+
+//session coockie expiration
+define('COOKIE_EXP_TIME', 2592000); // 30 days in seconds
